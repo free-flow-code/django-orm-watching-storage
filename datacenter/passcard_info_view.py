d2df29ef53539d9724e65c07a0670f1b462a986b
free-flow-code <wildmuskaria8@gmail.com>
2023-04-11 16:25:32 +0400

    The display of visits for the passcard is configured.

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -3,17 +3,16 @@ from datacenter.models import Visit
 from django.shortcuts import render
 
 
+def get_passcard_visits(passcard):
+    all_visits = Visit.objects.filter(passcard=passcard)
+    return all_visits[::-1]
+
+
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.all()[0]
     # Программируем здесь
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    this_passcard_visits = get_passcard_visits(passcard)
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
