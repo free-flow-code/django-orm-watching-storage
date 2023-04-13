a3e513e56b676b4ffc9d744e5e195b04721e17db
free-flow-code <wildmuskaria8@gmail.com>
2023-04-13 20:15:13 +0400

    Removed comments.

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -7,6 +7,6 @@ from django.shortcuts import get_list_or_404
 def active_passcards_view(request):
     active_passcards = get_list_or_404(Passcard, is_active=True)
     context = {
-        'active_passcards': active_passcards,  # люди с активными пропусками
+        'active_passcards': active_passcards,
     }
     return render(request, 'active_passcards.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -41,6 +41,6 @@ def storage_information_view(request):
         print(details_visit)
 
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
