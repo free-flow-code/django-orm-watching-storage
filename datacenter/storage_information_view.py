f82e4b69d37ea03c63e254b2e0071642e471d04b
free-flow-code <wildmuskaria8@gmail.com>
2023-04-28 15:33:38 +0400

    Removed extra imports.

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,3 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from datacenter.time_processing import get_duration
 from datacenter.time_processing import format_duration
