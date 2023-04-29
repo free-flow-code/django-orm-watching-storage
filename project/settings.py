7719b11849697b9836eba0b2802c65a4e8af4642
free-flow-code <wildmuskaria8@gmail.com>
2023-04-29 23:02:48 +0400

    The default settings are set.

--- project/settings.py
+++ project/settings.py
@@ -19,11 +19,11 @@ INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', default='False')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
